ef7b07aa694e8a27e212768a7ced34dfa2be16fe
henrotaym <mathieu.henrotay@gmail.com>
2025-09-07 02:29:14 +0200

    🐛 Wrong message displayed.

--- src/main/java/henrotaym/env/http/controllers/HelloWorldController.java
+++ src/main/java/henrotaym/env/http/controllers/HelloWorldController.java
@@ -1,15 +1,15 @@
 package henrotaym.env.http.controllers;
 
-import henrotaym.env.enums.ProfileName;
-import henrotaym.env.queues.emitters.Emitter;
-import henrotaym.env.queues.events.HelloWorldEvent;
-import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import henrotaym.env.enums.ProfileName;
+import henrotaym.env.queues.emitters.Emitter;
+import henrotaym.env.queues.events.HelloWorldEvent;
+import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
@@ -20,8 +20,8 @@ public class HelloWorldController {
 
   @GetMapping("")
   public ResponseEntity<String> hello() {
-    this.emitter.send(new HelloWorldEvent("hello world event"));
+    this.emitter.send(new HelloWorldEvent("hello world event 2"));
 
-    return ResponseEntity.status(HttpStatus.OK).body("hello world");
+    return ResponseEntity.status(HttpStatus.OK).body("hello world 2");
   }
 }
